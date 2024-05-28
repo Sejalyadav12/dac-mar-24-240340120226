8537e7e56bffc72777c0e6dab69d167e97c44512
Sejalyadav12 <90088618+Sejalyadav12@users.noreply.github.com>
2024-05-28 22:24:43 +0530

    Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,9 +2,9 @@ class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello world");
 <<<<<<< HEAD
-		System.out.println("This is branch B-1"):wq;
+		System.out.println("This is branch B-1");
 =======
-		System.out.println("This is branch B-2");ii
+		System.out.println("This is branch B-2");
 >>>>>>> B-2
 	}
 }
